26c2ecb357e8b8988eeaced7bd43724457920555
golods96 <golods@gmail.com>
2013-03-15 23:36:58 -0400

    Initial Commit

---
+++ readResponse/READAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  READAppDelegate.h
+//  readResponse
+//
+//  Created by Lev on 3/15/13.
+//  Copyright (c) 2013 96 Bytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class READViewController;
+
+@interface READAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) READViewController *viewController;
+
+@end
---
+++ readResponse/READViewController.m
@@ -0,0 +1,29 @@
+//
+//  READViewController.m
+//  readResponse
+//
+//  Created by Lev on 3/15/13.
+//  Copyright (c) 2013 96 Bytes. All rights reserved.
+//
+
+#import "READViewController.h"
+
+@interface READViewController ()
+
+@end
+
+@implementation READViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
