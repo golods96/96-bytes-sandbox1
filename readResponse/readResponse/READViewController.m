53f18175d9716ecdd1ff81c1ecc825e6579aa3d3
golods96 <golods@gmail.com>
2013-03-16 23:43:14 -0400

    First Commit

---
+++ readResponse/readResponse/READViewController.m
@@ -0,0 +1,53 @@
+//
+//  READViewController.m
+//  readResponse
+//
+//  Created by Lev on 3/15/13.
+//  Copyright (c) 2013 96 Bytes. All rights reserved.
+//
+
+#import "READViewController.h"
+#import "readView.h"
+
+@interface READViewController ()
+
+@end
+
+@implementation READViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) loadView{
+    [super loadView];
+    CGRect frame = CGRectMake(10, 10, 300, 300);
+    
+    readView*   myView = [[readView alloc ] initWithFrame:frame] ;
+    
+    myView.clipsToBounds = YES;
+    [[self view] addSubview:myView];
+    
+    UIButton* myButton = [[UIButton alloc] initWithFrame:frame];
+
+    [myButton setTitle:@"unpressed" forState:UIControlStateNormal];
+    [myButton addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [[self view] addSubview:myButton ];
+    
+}
+
+- (void)buttonClicked:(UIButton*)button
+{
+    NSLog(@"clicked");
+}
+
+@end
