ea3d210d5a39316562e929ecfe96271afb364660
golods96 <golods@gmail.com>
2013-03-27 23:56:07 -0400

    Adding parsing framework

---
+++ readResponse/readResponse/READParser.h
@@ -0,0 +1,16 @@
+//
+//  READParser.h
+//  readResponse
+//
+//  Created by Lev on 3/27/13.
+//  Copyright (c) 2013 96 Bytes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface READParser : NSObject
+
+
+- (void) parseData: (NSData *) receivedData;
+
+@end
---
+++ readResponse/readResponse/READParser.m
@@ -0,0 +1,22 @@
+//
+//  READParser.m
+//  readResponse
+//
+//  Created by Lev on 3/27/13.
+//  Copyright (c) 2013 96 Bytes. All rights reserved.
+//
+
+#import "READParser.h"
+
+@implementation READParser
+
+
+- (void) parseData: (NSData *) receivedData
+{
+    NSLog(@"prepared to parse");
+    
+
+    NSLog(@"str: %@", receivedData);
+}
+
+@end
